c8c7d5124e5418092da7c596061292c8dc6f1eec
premajanakkumar <premajanakkumar@gmail.com>
2023-02-20 18:19:32 +0530

    req

--- test_calculator.py
+++ test_calculator.py
@@ -5,6 +5,7 @@ class TestCalculator:
 
 
     def test_addition(self):
-       assert 4 == calculator.add(2, 2)
+        assert 4 == calculator.add(2, 2)
+
     def test_subtraction(self):
-       assert 2 == calculator.subsract(4, 2)
+        assert 2 == calculator.subsract(4, 2)
