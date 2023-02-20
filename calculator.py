9fe52cfe48c5e5abda9d9f6b6589c029dbf56040
premajanakkumar <premajanakkumar@gmail.com>
2023-02-20 15:24:50 +0530

    python app

---
+++ calculator.py
@@ -0,0 +1,12 @@
+"""
+Calculator library containing basic math operations
+
+"""
+
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subract(first_term, second_term):
+    return first_term - second_term
