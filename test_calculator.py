480be712932bd66e46847e882e44ea42b7a4db8a
premajanakkumar <premajanakkumar@gmail.com>
2023-02-20 17:47:26 +0530

    req

--- test_calculator.py
+++ test_calculator.py
@@ -2,6 +2,7 @@ import calculator
 
 class TestCalculator:
     def test_addition(self):
-        assert 4 == calculator.add(2,2)
+        assert 4 == calculator.add(2, 2)
     def test_addition(self):
-        assert 2 == calculator.subsract(4,2)
+        assert 2 == calculator.subsract(4, 2)
+
