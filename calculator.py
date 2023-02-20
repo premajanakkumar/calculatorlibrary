bdf193a16d5ef893828ad784141f3831c7661d2c
premajanakkumar <premajanakkumar@gmail.com>
2023-02-20 18:08:09 +0530

    req

--- calculator.py
+++ calculator.py
@@ -7,8 +7,9 @@ Calculator library containing basic math operations
 def add(first_term, second_term):
     return first_term + second_term
 
-
 def subsract(first_term, second_term):
     return first_term - second_term
+
+
 
 
